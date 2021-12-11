98540a0d8fed2d7d91537275fcc6500dcd41cc3b
Junior Felix <33384019+juniorfelixgb@users.noreply.github.com>
2021-12-11 12:00:11 -0400

    integrando fluent validation -> PROGRESS

---
+++ src/PlannerApp.Shared/Request/LoginRequest.cs
@@ -0,0 +1,19 @@
+﻿// ********************************************************************** 
+//  ** CodingWithJunior - PlannerApp Application v1.0.0 
+//  ** Copyright (c) 2021 Microsoft Corporation 
+//  *********************************************************************
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlannerApp.Shared.Request
+{
+    public partial class LoginRequest
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ src/PlannerApp/Components/Authentication/LoginForm.razor.cs
@@ -0,0 +1,14 @@
+﻿// ********************************************************************** 
+//  ** CodingWithJunior - PlannerApp Application v1.0.0 
+//  ** Copyright (c) 2021 Microsoft Corporation 
+//  *********************************************************************
+
+using Microsoft.AspNetCore.Components;
+
+namespace PlannerApp.Components.Authentication
+{
+    public partial class LoginForm : ComponentBase
+    {
+
+    }
+}
